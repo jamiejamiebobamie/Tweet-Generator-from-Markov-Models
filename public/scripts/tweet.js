812b375d0c273e7b6a3bdaa3c0fe5f1d6df5d638
jamiejamiebobamie <jmccrory@vt.edu>
2019-05-06 22:56:11 -0700

    refactored tweet.js

--- public/scripts/tweet.js
+++ public/scripts/tweet.js
@@ -1,57 +1,66 @@
 const fs = require("fs");
 
 function chooseRandomFile(){
-    var files = ["Wilde.md", "Shakespeare.md", "Grimm.md", "Carroll.md", "Lovecraft.md", "Woolf.md", "Poe.md"];
+    let files = ["Wilde.md", "Shakespeare.md", "Grimm.md", "Carroll.md", "Lovecraft.md", "Woolf.md", "Poe.md"];
     return files[Math.floor(Math.random() * Math.floor(files.length))];
 }
 
+// function fileToArray(file){
+//     file = __dirname + "/corpi/" + file
+//     var text = fs.readFileSync(file).toString('utf-8');
+//     var words = text.split(" ")
+//     return words
+// }
+
 function fileToArray(file){
-    file = __dirname + "/corpi/" + file
-    // console.log(file)
-    var text = fs.readFileSync(file).toString('utf-8');
-    var words = text.split(" ")
+    //for testing...
+    file = "/Users/jamesmccrory/Documents/dev/tweet-gen-js/public/scripts/corpi/" + file
+    let text = fs.readFileSync(file).toString('utf-8');
+    let words = text.split(" ")
     return words
 }
 
  function arrayToString(array){
-     var string = ""
-     var i;
-     for (i = 0; i < array.length; i++) {
+     let string = ""
+     for (let i = 0; i < array.length; i++) {
          string += array[i].toString('utf-8') + " "}
      return string;
 }
 
 function wordBeforeAfter(array, word, n) {
-   var instances = []
-   for (var i = 0; i < array.length; i++) {
+// Takes in an array of words in order from a text, a target word, and an integer of how many words to look before the target
+   let instances = []
+   for (let i = 0; i < array.length; i++) {
        if (array[i] == word){
-           var x = i - 1;
-           var beforeWords = [];
-           while (x > i - n) {
-            beforeWords.push(array[x]);
-                x -= 1;
+           let x = i - 1; //the first word before the target word
+           let beforeWords = [];//the array of words to come before the target
+           if (x > 0){ // in case x is greater than the first index of the input array
+               while (x > i - n) {// n == number of words to look back "in time"
+                    beforeWords.push(array[x]);//push 'n' number of words into the array
+                    x -= 1;//increment x
                 }
-                var myInstance = [word, beforeWords, array[i+1]];
-                instances.push(myInstance)
-                }
-             }
-             return instances
-           }
+            }
+            let myInstance = [word, beforeWords, array[i+1]]; //creates an entry into instances.
+            // an entry in instances consists of a target word:
+            // myInstance[0] == a target word
+            // myInstance[1] == 'n' words that come before the target
+            // myInstance[2] == the next word that comes after the target
+            instances.push(myInstance)
+        }
+    }
+    return instances
+}
 
 function nextWords(instances) {
-    var lenInt = 0
-    var next = {};
-    for (var i = 0; i < instances.length; i++) {
+    let next = {};
+    for (let i = 0; i < instances.length; i++) {
         if (instances[i][2] in next) {
             next[instances[i][2]] += 1;
         } else {
             next[instances[i][2]] = 1
         }
-        if (lenInt < next[instances[i][2]]){
-            lenInt = next[instances[i][2]]
-        };
     }
-    return ([next, lenInt])
+    return next
 };
 
 function pickRand(nexts){
@@ -64,10 +73,9 @@ function pickRand(nexts){
 }
 
 function valuestoKeys(nexts){
-    var max = nexts[1];
     var newDict = {}
-    var arrKeys = Object.keys(nexts[0])
-    var arrValues = Object.values(nexts[0])
+    var arrKeys = Object.keys(nexts)
+    var arrValues = Object.values(nexts)
     for (var i = 0; i < arrValues.length; i++){
         if (arrValues[i] in newDict) {
         newDict[arrValues[i]].push(arrKeys[i]);
@@ -75,6 +83,7 @@ function valuestoKeys(nexts){
         newDict[arrValues[i]] = [arrKeys[i]]
     }
     }
+    // console.log(newDict)
     return newDict
 }
 
@@ -108,12 +117,7 @@ function check(tweet){ //slows it down like crazy and all of the if statements d
         }
         if (i == (tweet.length-1) && tweet[i] == ",") {
                 tweet = tweet.substring(0, tweet.length-1);
-                console.log(tweet[i])
-        // } else if (i == (tweet.length-1) && punc.includes(tweet[i]) ){
-            //
-        // } else if (i == (tweet.length-1) && punc.includes(tweet[i]) ){
-            // tweet = tweet.charAt(0).toUpperCase() + tweet.slice(1);
-            // tweet = tweet + "."
+                // console.log(tweet[i])
         } else if (i == (tweet.length-1) && punc.includes(tweet[i])) {
             tweet = tweet;
         } else {
@@ -128,153 +132,21 @@ function check(tweet){ //slows it down like crazy and all of the if statements d
     }
     return [tweet, even]
 }}
-//
-// module.exports.chooseRandomFile = function (){
-//     var files = ["Wilde.md", "Shakespeare.md", "Grimm.md", "Carroll.md", "Lovecraft.md", "Woolf.md", "Poe.md"];
-//     return files[Math.floor(Math.random() * Math.floor(files.length))];
-// }
-//
-// module.exports.fileToArray = function (file){
-//     var text = fs.readFileSync(file).toString('utf-8');
-//     var words = text.split(" ")
-//     return words
-// }
-//
-// module.exports.arrayToString = function (array){
-//      var string = ""
-//      var i;
-//      for (i = 0; i < array.length; i++) {
-//          string += array[i].toString('utf-8') + " "}
-//      return string;
-// }
-//
-// module.exports.wordBeforeAfter = function (array, word, n) {
-//    var instances = []
-//    for (var i = 0; i < array.length; i++) {
-//        if (array[i] == word){
-//            var x = i - 1;
-//            var beforeWords = [];
-//            while (x > i - n) {
-//             beforeWords.push(array[x]);
-//                 x -= 1;
-//                 }
-//                 var myInstance = [word, beforeWords, array[i+1]];
-//                 instances.push(myInstance)
-//                 }
-//              }
-//              return instances
-//            }
-//
-// module.exports.nextWords = function (instances) {
-//     var lenInt = 0
-//     var next = {};
-//     for (var i = 0; i < instances.length; i++) {
-//         if (instances[i][2] in next) {
-//             next[instances[i][2]] += 1;
-//         } else {
-//             next[instances[i][2]] = 1
-//         }
-//         if (lenInt < next[instances[i][2]]){
-//             lenInt = next[instances[i][2]]
-//         };
-//     }
-//     return ([next, lenInt])
-// };
-//
-// module.exports.pickRand = function (nexts){
-//     var rand = Math.floor(Math.random() * Math.floor(nexts[1]));
-//     if (rand != 0){
-//         return rand;
-//     } else {
-//         return 1;
-//     }
-// }
-//
-// module.exports.valuestoKeys = function (nexts){
-//     var max = nexts[1];
-//     var newDict = {}
-//     var arrKeys = Object.keys(nexts[0])
-//     var arrValues = Object.values(nexts[0])
-//     for (var i = 0; i < arrValues.length; i++){
-//         if (arrValues[i] in newDict) {
-//         newDict[arrValues[i]].push(arrKeys[i]);
-//     } else {
-//         newDict[arrValues[i]] = [arrKeys[i]]
-//     }
-//     }
-//     return newDict
-// }
-//
-// var punc = [".", "!", "?", ";", ","]
-//
-// var myAuthors = {
-//     "Poe": "Edgar Allan Poe",
-//     "Lovecraft": "HP Lovecraft",
-//     "Woolf": "Virginia Woolf",
-//     "Shakespeare": "William Shakespeare",
-//     "Carroll": "Lewis Carroll",
-//     "Wilde": "Oscar Wilde",
-//     "Grimm": "The Brothers Grimm",
-// }
-//
-// module.exports.check = function (tweet){ //slows it down like crazy and all of the if statements don't work properly...
-//     var even = false;
-//     var count = 0;
-//
-//     for (var i=0; i < (tweet.length); i++) {
-//
-//         if (i == 0 && tweet[i] == " ") {
-//             tweet = tweet.substring(1, tweet.length);
-//         }
-//
-//         if (tweet[i] == "\"") {
-//             count++;
-//         }
-//         if (tweet.substring(tweet.length-3, tweet.length-1) == "and") {
-//             tweet = tweet + "...";
-//         }
-//         if (i == (tweet.length-1) && tweet[i] == ",") {
-//                 tweet = tweet.substring(0, tweet.length-1);
-//                 console.log(tweet[i])
-//         // } else if (i == (tweet.length-1) && punc.includes(tweet[i]) ){
-//             //
-//         // } else if (i == (tweet.length-1) && punc.includes(tweet[i]) ){
-//             // tweet = tweet.charAt(0).toUpperCase() + tweet.slice(1);
-//             // tweet = tweet + "."
-//         } else if (i == (tweet.length-1) && punc.includes(tweet[i])) {
-//             tweet = tweet;
-//         } else {
-//             tweet = tweet.charAt(0).toUpperCase() + tweet.slice(1);
-//             tweet = tweet + ".";
-//         }
-//
-//     if (count%2 != 0 && count != 0) {
-//         even = true;
-//     } else {
-//         even = false;
-//     }
-//     return [tweet, even]
-// }}
 
 module.exports.run = function (notRandom){
-var n = 6;
-var files = ["Grimm.md", "Poe.md", "Wilde.md", "Woolf.md", "Carroll.md", "Shakespeare.md", "Lovecraft.md"];
-var file = files[notRandom]
-// } else {
-//     file = chooseRandomFile();
-// }
-
-var fileArray = fileToArray(file);
-var word = fileArray[Math.floor(Math.random() * Math.floor(fileArray.length))]
-var tweet = word
+let n = 6;
+let files = ["Grimm.md", "Poe.md", "Wilde.md", "Woolf.md", "Carroll.md", "Shakespeare.md", "Lovecraft.md"];
+let file = files[notRandom]
+let fileArray = fileToArray(file);
+let word = fileArray[Math.floor(Math.random() * Math.floor(fileArray.length))]
+let tweet = word
 
 while (tweet.length < 110) {
-    var wBA = wordBeforeAfter(fileArray, word, n); // file to array of words
-    var nexts = nextWords(wBA); // next words
-    var max = nexts[1] // max frequency (the frequency of the most likely next word)
-    var vTK = valuestoKeys(nexts); // dictionary of frequencies and arrays of words
-    var keysvTK = Object.keys(vTK) // array of keys of frequencies
-    var v = Math.floor(Math.random() * Math.floor(keysvTK.length)); // random index out of the array of keys of frequencies
+    let arrayOfInstances = wordBeforeAfter(fileArray, word, n); // file to array of words
+    let nexts = nextWords(arrayOfInstances); // next words
+    let valuesAsKeys = valuestoKeys(nexts); // dictionary of frequencies and arrays of words
+    let keysvTK = Object.keys(valuesAsKeys) // array of keys of frequencies
+    let value = Math.floor(Math.random() * Math.floor(keysvTK.length)); // random index out of the array of keys of frequencies
     word = vTK[keysvTK[v]][Math.floor(Math.random() * Math.floor(vTK[keysvTK[v]].length - 1))] // random index out of the array of words for frequency 'v'
     tweet = tweet + " " + word // 'word' NOT WEIGHTED -- HOW TO IMPLEMENT??
 }
@@ -295,3 +167,56 @@ if (odd){
 
 return tweet
 }
+
+function run(notRandom){
+let n = 7;
+let files = ["Grimm.md", "Poe.md", "Wilde.md", "Woolf.md", "Carroll.md", "Shakespeare.md", "Lovecraft.md"];
+let file = files[notRandom]
+
+let fileArray = fileToArray(file);
+let word = fileArray[Math.floor(Math.random() * Math.floor(fileArray.length))]
+let tweet = word
+
+while (tweet.length < 110) {
+    let arrayOfInstances = wordBeforeAfter(fileArray, word, n); // file to array of words
+    let nexts = nextWords(arrayOfInstances); // next words
+    let valuesDictionary = valuestoKeys(nexts); // dictionary of key: frequencies and value: arrays of words
+    let values = Object.keys(valuesDictionary) // array of keys of frequencies
+    let frequentWords = []
+    if (values.length >= 7){// if there's 5 or more options, pick the most frequent words...
+        let i = 0;
+        while (i < 7){
+            // console.log(values, values[values.length-i-1], valuesDictionary[values[values.length-i-1]])
+            frequentWords = frequentWords.concat(valuesDictionary[values[values.length-i-1]])
+            // console.log(frequentWords)
+            i+=1
+        }
+        let random_index_into_frequentWords = Math.floor(Math.random() * Math.floor(frequentWords.length)); // random index out of the array of keys of frequencies
+        word = frequentWords[random_index_into_frequentWords]
+        // console.log(word)
+    } else { // if the frequency is '1', pick a word from that frequency array...
+        let random_index_into_values = Math.floor(Math.random() * Math.floor(values.length)); // random index out of the array of keys of frequencies
+        word = valuesDictionary[values[random_index_into_values]][Math.floor(Math.random() * Math.floor(valuesDictionary[values[random_index_into_values]].length - 1))] // random index out of the array of words for frequency 'v'
+    }
+    tweet = tweet + " " + word
+}
+
+var author = file.slice(0, -3)
+
+var thing = check(tweet)
+var odd = thing[1]
+tweet = thing[0]
+
+if (odd){
+    tweet = tweet + "\"" + " -" + myAuthors[author];
+} else {
+    tweet = tweet + " -" + myAuthors[author];
+}
+
+ tweet = tweet.charAt(0).toUpperCase() + tweet.slice(1);
+
+return tweet
+}
+
+
+console.log(run(0))
--- public/scripts/tweet_handlebars_interactivity.js
+++ public/scripts/tweet_handlebars_interactivity.js
@@ -210,6 +210,29 @@ if ("lovecraft" != on && lovecraftBool == true){
 
 }
 
+// $( function() {
+// 		$( ".jar_image" ).mgGlitch({
+//           // set 'true' to stop the plugin
+// 				  destroy : false,
+//           // set 'false' to stop glitching
+//           glitch: true,
+//           // set 'false' to stop scaling
+//           scale: true,
+//           // set 'false' to stop glitch blending
+//           blend : true,
+//           // select blend mode type
+//           blendModeType : 'hue',
+//           // set min time for glitch 1 elem
+//           glitch1TimeMin : 1000,
+//           // set max time for glitch 1 elem
+//           glitch1TimeMax : 1400,
+//           // set min time for glitch 2 elem
+//           glitch2TimeMin : 30,
+//           // set max time for glitch 2 elem
+//           glitch2TimeMax : 50,
+// 		});
+// });
+
 function goHere(){
         // location.href='/tweet';
         location.href='new_tweet/:msg';
